95dfad56871560dd120310510a5bef624570b5be
熊超 <chao.xiong@9m-tec.com>
2022-08-15 16:14:08 +0800

    test: 调整示例

--- test/test_env_config.py
+++ test/test_env_config.py
@@ -7,7 +7,7 @@ from silas.env import Env, EnvConfig
 class MyEnvConfig(EnvConfig):
     """ test 1 class
     """
-    name: str = Env('xc', mp={'x': 'father'}, use_prefix=False, un_match_mp=True)
+    name: str = Env('human', mp={'human': 'xc', 'animal': 'pig'}, use_prefix=True, un_match_mp=True)
     age: int = Env(19, use_prefix=False)
     ID = 1
     sex: str
@@ -18,13 +18,16 @@ class MyEnvConfig(EnvConfig):
 
 
 os.environ["auth_name"] = '123'
-os.environ["auth_sex"] = '1'
+os.environ["age"] = '20'
+os.environ["auth_sex"] = '男'
 os.environ["auth_begin_date"] = '2020-01-01 00:00:00'
 
 config = MyEnvConfig()
 print(config.g)
 
+os.environ["auth_sex"] = '无性别'
 sex = config.present_get('sex')
+print(config.g.sex)
 print(sex)
 
 begin_date = config.present_get('begin_date')
